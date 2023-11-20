caa4185932292ad6b2818ccbd1a9313f3746036c
yeetengm <yeeteng11.kl@gmail.com>
2023-11-20 16:50:56 +0800

    update numpy class

---
+++ W6/nump1.py
@@ -0,0 +1,16 @@
+
+import numpy as np
+
+# 1. a = np.array([1,2,3,4,5,6,7,8,9])
+#    print(a)                                Output: [1 2 3 4 5 6 7 8 9]
+
+# 2. As a range 
+#    a = np.arange(10)
+#    print(a)                               Output: [0 1 2 3 4 5 6 7 8 9]
+
+# 3. Specifying numbers of elements
+#    a = np.linspace(0,1,5)
+#    print(a)                               Output: [0.   0.25 0.5  0.75 1.  ]
+
+
+
